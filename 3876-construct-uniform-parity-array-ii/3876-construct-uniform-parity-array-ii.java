123199352b4a7f072b254df677f086aa75357652
Atul Sharma <atulsharma5393@gmail.com>
2026-09-03 22:38:21 +0530

    Time: 5 ms (83.33%), Space: 121.5 MB (74.64%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+        int min = nums1[0];
+        boolean odd = false;
+
+        for (int x : nums1) {
+            min = Math.min(min, x);
+
+            if (x % 2 != 0) {
+                odd = true;
+            }
+        }
+
+        if (min % 2 == 0 && odd) {
+            return false;
+        }
+
+        return true;
+    }
+}
