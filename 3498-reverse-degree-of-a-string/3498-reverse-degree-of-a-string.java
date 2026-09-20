a47818caf38d442c620a176b22fe0b6c078e6d60
Atul Sharma <atulsharma5393@gmail.com>
2026-09-20 21:34:34 +0530

    Time: 1 ms (100%), Space: 44.2 MB (18.31%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum =0;
+        for(int i=1;i<=s.length();i++){
+            char ch= s.charAt(i-1);
+            sum += ('z'-ch +1)*i;
+        }
+        return sum;
+    }
+}
